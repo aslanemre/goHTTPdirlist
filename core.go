c2e38bc13f1d6e9c60df91d92ced89fae0d1b005
EMRE ASLAN <52652572+aslanemre@users.noreply.github.com>
2021-12-30 14:55:12 +0300

    Add files via upload

---
+++ core.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"mux"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Write([]byte("<title>Ansayfa</title>\n"))
+	w.Write([]byte("<center><h1>Anasayfa</h1></center><br>\n"))
+	w.Write([]byte("<center><h3><a href='/listele'>Listele</a></h3></center><br>"))
+}
+func listele(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Write([]byte("<title>20703043-Dizin Listeleme</title>\n"))
+	w.Write([]byte("<h1>Dizin Listeleme</h1><br>\n"))
+	files, _ := ioutil.ReadDir("./")
+	for _, f := range files {
+		if f.IsDir() {
+			w.Write([]byte("dizin>_\t" + f.Name() + "<br>\n"))
+		} else {
+			w.Write([]byte("dosya>_\t" + f.Name() + "<br>\n"))
+		}
+	}
+}
+func main() {
+	router := mux.NewRouter()
+	router.Headers("Content-Type", "text/html")
+	router.HandleFunc("/listele", listele).Methods("GET")
+	router.HandleFunc("/", home).Methods("GET")
+	server := &http.Server{
+		Handler: router,
+		Addr:    "127.0.0.1:1337",
+	}
+	fmt.Println("Server http://localhost:1337/ adresinde başlatıldı.")
+	log.Fatal(server.ListenAndServe())
+}
---
+++ server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func anasayfa(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Write([]byte("<title>Ansayfa</title>\n"))
+	w.Write([]byte("<center><h1>Anasayfa</h1></center><br>\n"))
+	w.Write([]byte("<center><h3><a href='/listele'>Listele</a></h3></center><br>"))
+}
+func listeleme(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Write([]byte("<title>20703043-Dizin Listeleme</title>\n"))
+	w.Write([]byte("<h1>Dizin Listeleme</h1><br>\n"))
+	files, _ := ioutil.ReadDir("./")
+	for _, f := range files {
+		if f.IsDir() {
+			w.Write([]byte("dizin>_\t" + f.Name() + "<br>\n"))
+		} else {
+			w.Write([]byte("dosya>_\t" + f.Name() + "<br>\n"))
+		}
+	}
+}
+func main() {
+	http.HandleFunc("/listele", listeleme)
+	http.HandleFunc("/", anasayfa)
+	server := &http.Server{
+		Addr: "127.0.0.1:1337",
+	}
+	fmt.Println("Server http://localhost:1337/ adresinde başlatıldı.")
+	log.Fatal(server.ListenAndServe())
+}
